209e6a8a7b1b50b384e6dfd41b5785f5fc3bdc52
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-10-26 12:30:27 +0500

    Task5

---
+++ Tyuiu.NosovaVD.Sprint3.Task5.V5.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.NosovaVD.Sprint3.Task5.V5.Lib
+{
+    public class DataService : ISprint3Task5V5
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double res = 0;
+            for(int i = startValue1; i <= stopValue1; i++)
+            {
+                for(int j = startValue2; j <= stopValue2; j++)
+                {
+                    res += (x / Math.Sin(j));
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.NosovaVD.Sprint3.Task5.V5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NosovaVD.Sprint3.Task5.V5.Lib;
+namespace Tyuiu.NosovaVD.Sprint3.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Носова В.Д. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Носова Валерия Дмитриевна | ПКТб-23-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя вложенный цикл, которая вычисляет сумму    *");
+            Console.WriteLine("* ряда по формуле, при x = 5                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+            Console.WriteLine("Переменная x: " + x);
+            Console.WriteLine("Старт шага первой суммы ряда: " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда: " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда: " + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда: " + stopValue2);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            Console.ReadKey();
+        }
+    }
+}
