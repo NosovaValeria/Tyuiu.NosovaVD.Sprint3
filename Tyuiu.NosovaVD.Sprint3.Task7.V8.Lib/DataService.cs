0ab4f116197d2bb5844ede8ca2a0b5149944ad87
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-10-26 14:04:15 +0500

    Task7

---
+++ Tyuiu.NosovaVD.Sprint3.Task7.V8.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.NosovaVD.Sprint3.Task7.V8.Lib
+{
+    public class DataService : ISprint3Task7V8
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] vallueArray;
+            int len = (stopValue - startValue) + 1;
+            vallueArray = new double[len];
+            double y;
+            int count = 0;
+            for(int x = startValue; x <= stopValue; x++)
+            {
+                if((2-x) == 0)
+                {
+                    vallueArray[count] = 0;
+                    count++;
+                }
+                else
+                {
+                    y = Math.Round((Math.Sin(x) + ((Math.Cos(x) + 1) / (2 - x)) + 2 * x), 2);
+                    vallueArray[count] = y;
+                    count++;
+                }             
+            }
+            return vallueArray;
+        }
+    }
+}
---
+++ Tyuiu.NosovaVD.Sprint3.Task7.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NosovaVD.Sprint3.Task7.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NosovaVD.Sprint3.Task7.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5149727f-2296-4e73-a793-67cf14c833a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
